92cefb5d1f3920e5846a6f5c552d44543915fd20
vishal5394 <vishaljadyal@gmail.com>
2022-05-29 18:52:10 +0530

    Leap year'

---
+++ Leapyear.java
@@ -0,0 +1,19 @@
+package core_program;
+
+import java.util.Scanner;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		Scanner LP = new Scanner(System.in);
+		System.out.println("Enter the Year in 4 digit");
+		int n=LP.nextInt();
+		if ( n % 4 == 0 && n > 999)
+			System.out.println(n+" is leap year");
+		else if(n % 4 != 0 && n > 999)
+			System.out.println(n+" is not leap year");
+		else
+			System.out.println("error! you not enter 4 digit no");
+	}
+
+}
